5398a7de0e51ec5c6f74bc29e73b92dacfa4d8ad
Glória Lizane Marques Grecco <gloria.grecco@softdesign.com.br>
2024-02-08 19:46:13 -0300

    ajustes

--- codecept.conf.js
+++ codecept.conf.js
@@ -12,14 +12,25 @@ exports.config = {
   tests: './*_test.js', // os arquivos de testes sao os que tem esse sufixo
   output: './output',
   helpers: {
+    /*Appium: {
+      platform: 'Android',
+      app: 'C:\\Projetos_Outros\\Estudo\\Appium\\app-release.apk', //como fazer pra nao usar o caminho da minha maquina???
+      desiredCapabilities:{
+      appPackage: "com.qazandoapp",
+      appActivity: "MainActivity",
+        deviceName: "Pixel-7API33",
+        platformVersion: "13" 
+        //automationName: process.env.AUTOMATIONNAME  
+      },    
+    }*/
     Appium: {
-      platform: process.env.PLATFORM,
-      app: process.env.APP,
+      platform: process.env.PLATFORM ||'Android',
+      app: process.env.APP || 'C:\\Projetos_Outros\\Estudo\\Appium\\app-release.apk',
       desiredCapabilities: { 
-        deviceName: process.env.DEVICE,
-        platformVersion: process.env.VERSION,  //versao sistema operacional
-        appPackage: process.env.platform === 'Android' ? process.env.PACKAGE: "",
-        appActivity: process.env.platform === 'Android' ? process.env.ACTIVITY: ""
+        deviceName: process.env.DEVICE || 'Pixel-7API33' ,
+        platformVersion: process.env.VERSION || '13',  //versao sistema operacional
+        appPackage: process.env.platform === 'Android' ? process.env.PACKAGE: "" || 'com.qazandoapp',
+        appActivity: process.env.platform === 'Android' ? process.env.ACTIVITY: "" || 'MainActivity'
         //automationName: process.env.AUTOMATIONNAME  
       },    
     }
